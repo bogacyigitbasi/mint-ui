6346284a4161b41bbd938529e34979614ae2c895
Boğaçhan Yiğitbaşı <bogachanyigitbasi@Bogachans-MacBook-Air.local>
2022-12-26 17:10:44 +0300

    UI Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Initialize from "./Initialize";
+import Mint from "./Mint"
+import { useState } from "react";
+import { Container } from "@mui/material";
+export default function App() {
+  const [isConnected, setConnected] = useState(false);
+ return (
+  <div className="App">
+   <Header
+        onConnected={() => setConnected(true)}
+        onDisconnected={() => setConnected(false)}
+   />
+      
+      <Container sx={{ mt: 15 }}>
+   {isConnected && <Initialize/>}
+   {isConnected && <Mint/>}
+   </Container>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  </div>
+ );
 }
 
-export default App;
+
+}

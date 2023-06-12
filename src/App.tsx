d12e07769f6effd8d8382a1c5870a48c215874c6
Boğaçhan Yiğitbaşı <bogachanyigitbasi@Bogachans-MacBook-Air.local>
2023-06-12 16:26:08 +0300

    as smart contract parameters

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,19 @@ import Mint from "./Mint"
 import { useState } from "react";
 import { Container } from "@mui/material";
 export default function App() {
-  const [isConnected, setConnected] = useState(false);
- return (
-  <div className="App">
-   <Header
-        onConnected={() => setConnected(true)}
-        onDisconnected={() => setConnected(false)}
-   />
-      
-      <Container sx={{ mt: 15 }}>
-   {isConnected && <Initialize/>}
-   {isConnected && <Mint/>}
-   </Container>
+   const [isConnected, setConnected] = useState(false);
+   return (
+      <div className="App">
+         <Header
+            onConnected={() => setConnected(true)}
+            onDisconnected={() => setConnected(false)}
+         />
 
-  </div>
- );
-}
+         <Container sx={{ mt: 15 }}>
+            {isConnected && <Initialize />}
+            {isConnected && <Mint />}
+         </Container>
 
-
+      </div>
+   );
 }
